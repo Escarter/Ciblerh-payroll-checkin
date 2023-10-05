a20b126d67b25d2e3a1e4682a2768a7fad2b2ff6
Escarter <mbutuhescarter@gmail.com>
2023-10-05 10:35:25 +0100

    changed header

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,15 +15,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>{{$header ?? 'CibleRH - Checkin System'}}</title>
+    <title>{{$header ?? 'WiMa HR'}}</title>
 
-    <link rel="icon" type="image/png" href="{{ asset('img/fav.jpg')}}">
-    <link rel="icon" href="{{ asset('img/fav-32x32.png')}}" sizes="32x32">
-    <link rel="icon" href="{{ asset('img/fav-192x192.png')}}" sizes="192x192">
-    <link rel="apple-touch-icon" href="{{ asset('fav-180x180.png')}}">
-    <meta name="msapplication-TileImage" content="{{ asset('fav-270x270.png')}}">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
+
+    <meta name="msapplication-TileColor" content="#1F2937">
 
     <link type="text/css" href="{{ asset('vendor/sweetalert2/dist/sweetalert2.min.css')}}" rel="stylesheet">
     <link type="text/css" href="{{ asset('vendor/notyf/notyf.min.css')}}" rel="stylesheet">
